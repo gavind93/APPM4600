38ebfe28c00e017e2c992a3bd863b298432130f3
Gavin Doan <garvin@Gavins-MacBook-Pro.local>
2026-09-11 20:18:42 -0600

    Homework 2 Submission

---
+++ Homework/Homework_2/Question_3.py
@@ -0,0 +1,10 @@
+import math
+
+x=.0000000009999999995
+
+y=math.e**x
+print(y-1)
+
+y=x+(x**2)/2
+
+print(y)
---
+++ Homework/Homework_2/Question_6.py
@@ -0,0 +1,76 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(-3,10,.1)
+
+y = x - 4*np.sin(2*x)-3
+
+plt.plot(x,y)
+
+
+plt.axhline(0,color='k',linewidth=1)
+plt.axvline(0,color='k',linewidth=1)
+#plt.show()
+
+"""
+This script explores the use of the fixed point method.
+Two functions are considered that have different properties.
+I like to use this code before I talk about convergence analysis
+for the fixed point method as motivation.
+"""
+#############################################
+"""
+Copyright (C) 2025 Adrianna M. Gillman
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+#############################################
+# import libraries
+import numpy as np
+def driver():
+# test functions
+    f1 = lambda x: -1*np.sin(2*x)+5*x/4-(3/4)
+# fixed point is alpha1 = 1.4987....
+    #f2 = lambda x: 3+2*np.sin(x)
+#fixed point is alpha2 = 3.09...
+    Nmax = 100
+    tol = 1e-10
+# test f1 '''
+    x0 = 0.0
+    [xstar,ier] = fixedpt(f1,x0,tol,Nmax)
+    print('the approximate fixed point is:',xstar)
+    print('f1(xstar):',f1(xstar))
+    print('Error message reads:',ier)
+#test f2 '''
+    #x0 = 0.0
+    #[xstar,ier] = fixedpt(f2,x0,tol,Nmax)
+    #print('the approximate fixed point is:',xstar)
+    #print('f2(xstar):',f2(xstar))
+    #print('Error message reads:',ier)
+# define routines
+def fixedpt(f,x0,tol,Nmax):
+    ''' x0 = initial guess'''
+    ''' Nmax = max number of iterations'''
+    ''' tol = stopping tolerance'''
+
+    count = 0
+    while (count <Nmax):
+        count = count +1
+        x1 = f(x0)
+        if (abs(x1-x0) <tol):
+            xstar = x1
+            ier = 0
+            return [xstar,ier]
+        x0 = x1
+    xstar = x1
+    ier = 1
+    return [xstar, ier]
+driver()
